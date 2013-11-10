e9ea2d01031d457d8ea264d04376e34487f0ea6a
= <=>
2013-11-10 00:48:46 +0000

    Refactor home page to use a template

--- lists/tests.py
+++ lists/tests.py
@@ -4,6 +4,8 @@ from django.http import HttpRequest
 
 from lists.views import home_page
 
+from django.template.loader import render_to_string
+
 class HomePageTest(TestCase):
 	def test_root_url_resolves_to_home_page_view(self): 
 		found = resolve('/') 
@@ -12,6 +14,5 @@ class HomePageTest(TestCase):
 	def test_home_page_returns_correct_html(self):
 		request = HttpRequest()
 		response = home_page(request)
-		self.assertTrue(response.content.startswith(b'<html>'))
-		self.assertIn(b'<title>To-Do lists</title>', response.content)
-		self.assertTrue(response.content.endswith(b'</html>'))
+		expected_html = render_to_string('home.html')
+		self.assertEqual(response.content.decode(), expected_html)
