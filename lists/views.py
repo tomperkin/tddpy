b97284dc02f21fed1187a6726b4da6b9d4256318
= <=>
2013-11-14 16:33:27 +0000

    Try IntelliJ - changed tabs to spaces for some reason

--- lists/views.py
+++ lists/views.py
@@ -2,16 +2,17 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from lists.models import Item
 
+
 def home_page(request):
+    # If it's a POST, then create the new Item
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'])
+        return redirect('/lists/the-only-list-in-the-world/')
 
-	# If it's a POST, then create the new Item
-	if request.method == 'POST':
-		Item.objects.create(text=request.POST['item_text'])
-		return redirect('/lists/the-only-list-in-the-world/')
+    items = Item.objects.all()
+    return render(request, 'home.html')
 
-	items = Item.objects.all()
-	return render(request, 'home.html')
 
 def view_list(request):
-	items = Item.objects.all()
-	return render(request, 'list.html', {'items': items})
+    items = Item.objects.all()
+    return render(request, 'list.html', {'items': items})
