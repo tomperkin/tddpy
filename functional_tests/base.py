4b1fb2f5a579f749125d604060d263690ae6392f
= <=>
2013-11-18 15:09:28 +0000

    Refactor FT into multiple files

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -1,10 +1,8 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 import sys
 
-
-class NewVisitorTest(LiveServerTestCase):
+class FunctionalTest(LiveServerTestCase):
 
 	# Hack the inbuilt stuff to allow us to point the FT suite at a different server
 	@classmethod
@@ -35,113 +33,3 @@ class NewVisitorTest(LiveServerTestCase):
 		self.assertIn(row_text, [row.text for row in rows])
 
 
-	def test_can_start_a_list_and_retrieve_it_later(self):	
-		# Edith has heard about a cool new online to-do app. She goes 
-		# to check out its homepage
-		self.browser.get(self.server_url)
-
-		# She notices the page title and header mention to-do lists
-		self.assertIn('To-Do', self.browser.title)
-		header_text = self.browser.find_element_by_tag_name('h1').text
-		self.assertIn('To-Do', header_text)
-
-		# She is invited to enter a to-do item straight away
-		inputbox = self.browser.find_element_by_id('id_new_item')
-		self.assertEqual(
-			inputbox.get_attribute('placeholder'),
-			'Enter a To-Do item'
-		)
-
-		# She types "Buy peacock feathers" into a text box (Edith's hobby
-		# is tying fly-fishing lures)
-		inputbox.send_keys('Buy peacock feathers')
-
-		# When she hits enter she is taken to a new URL, and now the page lists
-		# "1: Buy peacock feathers" as an item in a to-do list
-		inputbox.send_keys(Keys.ENTER)
-		edith_list_url = self.browser.current_url
-		self.assertRegex(edith_list_url, '/lists/.+')
-
-		self.check_for_row_in_list_table('1: Buy peacock feathers')
-
-		# There is still a text box inviting her to add another item. She
-		# enters "Use peacock feathers to make a fly" (Edith is very methodical)
-		inputbox = self.browser.find_element_by_id('id_new_item')
-		inputbox.send_keys('Use peacock feathers to make a fly')
-		inputbox.send_keys(Keys.ENTER)
-
-		#import time # DBG
-		#time.sleep(2)
-
-		# The page updates again, and now shows both items on her list
-		self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
-		self.check_for_row_in_list_table('1: Buy peacock feathers')
-
-		# now a new user, Francis, comes along to the site - 
-		## kill Edith's browser to make
-		## sure that none of her state, cookies etc. is involved
-		self.browser.quit()
-		self.browser = webdriver.Firefox()
-
-		# Francis visits the home page. There is no sign of Edith's list
-		self.browser.get(self.server_url)
-		page_text = self.browser.find_element_by_tag_name('body').text
-		self.assertNotIn('Buy peacock feathers', page_text)
-		self.assertNotIn('make a fly', page_text)
-
-		# Francis starts a new list by entering a new item. he is less interesting than Edith
-		inputbox = self.browser.find_element_by_id('id_new_item')
-		inputbox.send_keys('Buy milk')
-		inputbox.send_keys(Keys.ENTER)
-
-		# Francis gets his own unique URL
-		francis_list_url = self.browser.current_url
-		self.assertRegex(francis_list_url, '/lists/.+')
-		self.assertNotEqual(francis_list_url, edith_list_url)
-
-		# Again, there is no trace of Edith's list
-		page_text = self.browser.find_element_by_tag_name('body').text
-		self.assertNotIn('Buy peacock feathers', page_text)
-		self.assertIn('Buy milk', page_text)
-
-		# Satisfied, she goes back to sleep
-
-	def test_layout_and_styling(self):
-		# Edith goes to the homepage
-		self.browser.get(self.server_url)
-		self.browser.set_window_size(1024,768)
-
-		# She notices that the input box is nicely centered
-		inputbox = self.browser.find_element_by_id('id_new_item')
-		self.assertAlmostEqual(
-			inputbox.location['x'] + inputbox.size['width'] /2,
-			512,
-			delta=3
-		)
-
-		# She starts a new list and that's nicely centred as well
-		inputbox.send_keys('testing\n')
-		inputbox = self.browser.find_element_by_id('id_new_item')
-		self.assertAlmostEqual(
-			inputbox.location['x'] + inputbox.size['width'] /2,
-			512,
-			delta=3
-		)
-
-
-
-		def test_cannot_add_empty_list_items(self):
-			# Edith goes to the home page and accidentally tries to submit
-			# an empty list item. She hits Enter on the empty input box
-
-			# The home page refreshes, and there is an error message saying
-			# that list items cannot be blank
-
-			# She tries again with some text for the item, which now works
-
-			# Perversely, she now decides to submit a second blank list item
-
-			# She receives a similar warning on the list page
-
-			# And she can correct it by filling some text in
-			self.fail('write me!')
